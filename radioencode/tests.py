bd03bef1741f99e0e5ad4ef593749ba7e79beec1
NeverMine17 <dannevergame@gmail.com>
2018-02-05 18:48:09 +0400

    dumb commit

--- radioencode/tests.py
+++ radioencode/tests.py
@@ -3,13 +3,13 @@ import unittest
 
 import numpy
 
-import radioencode
+from radioencode import Morse
 
 
 class RadioEncodeTest(unittest.TestCase):
     def test_enc(self):
-        arr = numpy.load(os.path.join('tests', 'data', 'hello.npy'))
-        morse = radioencode.Morse()
+        arr = numpy.load(os.path.join('radioencode', 'data', 'hello.npy'))
+        morse = Morse()
         self.assertTrue(numpy.allclose(
             arr, morse.encode('hello')
         ))
